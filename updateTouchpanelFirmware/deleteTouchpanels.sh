1f21a6eaad6568366ebde6695f7023eeba6e4bc3
Dan Clegg <dan_clegg@byu.edu>
2015-11-11 15:59:37 -0700

    Refactored update.py to use ElasticSearch client
    Former-commit-id: 3c5f49f6ce6849414f6f4f160c969cc468fc74a9
    Former-commit-id: ad90fbdd07d31067b87b024c7d27d957cef6ab8d

--- updateTouchpanelFirmware/deleteTouchpanels.sh
+++ updateTouchpanelFirmware/deleteTouchpanels.sh
@@ -3,4 +3,4 @@ i=1
 while read p; do
 	curl -XDELETE http://192.168.99.100:32781/devices/touchpanel/$i
 	i=$((i+1))
-done<touchpanels
+done<touchpanels.bak
--- updateTouchpanelFirmware/update.py
+++ updateTouchpanelFirmware/update.py
@@ -1,29 +1,13 @@
 #!/usr/bin/env python
 #update.py
-
-import requests
-import json
+from datetime import datetime
+from elasticsearch import Elasticsearch,RequestsHttpConnection
 
 host='192.168.99.100'
 port='32781'
+#url = 'http://{0}:{1}/devices/touchpanel/_search'.format(host,port)
+es = Elasticsearch(host='192.168.99.100',port='32781')
+res = es.get(index="devices", doc_type='touchpanel', id=1)
 
-url = 'http://{0}:{1}/devices/touchpanel/_search'.format(host,port)
-resp = requests.get(url)
-#print (myResponse.status_code)
+print(res['_source']['IPConfig']['IPAddress'])
 
-# For successful API call, response code will be 200 (OK)
-if(resp.ok):
-
-    # Loading the response data into a dict variable
-    # json.loads takes in only binary or string variables so using content to fetch binary content
-    # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
-    jData = json.loads(resp.content)
-    numPanels = jData['hits']['total']
-    if(numPanels > 0):
-    	print(numPanels)
-    else:
-    	print("Zilcho")
-    #print jData['hits']['hits'][0]['_source']['IP']
-else:
-  # If response code is not ok (200), print the resulting http error code with description
-    resp.raise_for_status()
